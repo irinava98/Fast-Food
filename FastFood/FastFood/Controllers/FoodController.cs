0bfd277e0774f33e326241d6f6d2ff3ca18cf48a
DESKTOP-AVOU6JD\Boris Kostov <ira98@abv.bg>
2022-11-09 19:40:41 +0200

    Adding first views and viewmodels

---
+++ FastFood/FastFood/Controllers/FoodController.cs
@@ -0,0 +1,26 @@
+﻿using FastFood.Contracts;
+using FastFood.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FastFood.Controllers
+{
+    public class FoodController:Controller
+    {
+        private readonly ICategoryService categoryService;
+        private readonly IFoodService foodService;
+
+        public FoodController(ICategoryService categoryService, IFoodService foodService)
+        {
+            this.categoryService = categoryService;
+            this.foodService = foodService;
+        }
+
+        public IActionResult All()
+        {
+            var model = new FoodsViewModel();
+            model.Foods = foodService.Foods;
+            model.CurrentCategory = "FoodCategory";
+            return View(model);
+        }
+    }
+}
---
+++ FastFood/FastFood/Models/FoodsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using FastFood.Data.Models;
+
+namespace FastFood.Models
+{
+    public class FoodsViewModel
+    {
+        public IEnumerable<Food> Foods { get; set; } = null!;
+        public string CurrentCategory { get; set; }=null!;
+    }
+}
