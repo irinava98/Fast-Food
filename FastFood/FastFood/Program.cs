137ea433b73eee2cec72231f55eaff9ebda4263f
DESKTOP-AVOU6JD\Boris Kostov <ira98@abv.bg>
2022-11-09 18:33:40 +0200

    Declaring our first services to the program.cs file

--- FastFood/FastFood/Program.cs
+++ FastFood/FastFood/Program.cs
@@ -1,4 +1,6 @@
+using FastFood.Contracts;
 using FastFood.Data;
+using FastFood.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +15,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IFoodService, FoodService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 var app = builder.Build();
 
